9b5fe6233104933bda841b7c4da59fee52fe735e
Ivan Gricyan <gricyan82@gmail.com>
2022-05-11 23:13:01 +0200

    feat: add arrows click effects and keyboard light switcher

--- src/script/container.js
+++ src/script/container.js
@@ -3,9 +3,9 @@ window.onload = function(){
   createCapsLockIndicator(); 
 };
 
-let textarea = document.getElementById("textarea");
-let keyBtns = document.querySelectorAll("input");
-
+const textarea = document.getElementById("textarea");
+const keyBtns = document.querySelectorAll("input");
+const LightMode = document.querySelector(".light-mode");
 
 const createCapsLockIndicator = () => {
   const capsLockIndicator = document.createElement("span");
@@ -22,6 +22,9 @@ for (let i = 0; i < keyBtns.length; i++) {
    // console.log("keyBtns[i]: ", keyBtns[i].dataset.attr);
    // console.log("event: ", event);
 
+   let previousText; 
+   let nextText; 
+   let caret;  
 
 
 
@@ -73,6 +76,16 @@ for (let i = 0; i < keyBtns.length; i++) {
       case "CapsLock":
         document.querySelector(".indicator").classList.toggle("key-up");
         break;
+      
+      case "ControlLeft":  
+      case "AltLeft": 
+      case "AltRight": 
+      case "ControlRight": 
+        break;
+
+      case "MetaLeft":
+        alert("The browser has limited access outside of itself for security reasons!"); 
+        break; 
 
       case "Enter":       
       if (textarea.selectionStart === textarea.selectionEnd) {
@@ -87,12 +100,70 @@ for (let i = 0; i < keyBtns.length; i++) {
         break;
 
       case "ShiftLeft":
-       // console.log("keyBtns[i]: ", keyBtns[i].dataset.attr);
-        break;
+      case "ShiftRight":
+        break;    
 
       case "Space":
         textarea.value += " ";
         break;
+
+      case "ArrowUp":        
+        for (let i = textarea.selectionStart - 1; i > 0; i -= 1) {
+          if (textarea.value[i] === "\n") {
+            for (let j = i + 1; j < textarea.value.length; j += 1) {
+              if (textarea.value[j] === "\n") {
+                caret = j - i;
+                break;
+              } else {
+                caret = textarea.value.length - i;
+              }
+            }
+            previousText = textarea.value.slice(0, i).split("\n");
+            previousText = previousText[previousText.length - 1].length;
+            if (previousText < caret && textarea.selectionStart - previousText > i) {
+              textarea.selectionStart -= textarea.selectionStart - i;
+              textarea.selectionEnd = textarea.selectionStart;
+            } else {
+              textarea.selectionStart -= previousText + 1;
+              textarea.selectionEnd = textarea.selectionStart;
+            }
+            break;
+          }
+        }
+        break;
+  
+      case "ArrowLeft":        
+        if (textarea.selectionStart !== 0) {
+          textarea.selectionStart -= 1;
+          textarea.selectionEnd = textarea.selectionStart;
+        }
+        break;
+  
+      case "ArrowRight":        
+        textarea.selectionStart += 1; 
+        break; 
+
+      case "ArrowDown":        
+        for (let i = textarea.selectionStart; i < textarea.value.length; i += 1) {
+          if (textarea.value[i] === "\n") {
+            for (let j = i - 1; j > 0; j -= 1) {
+              if (textarea.value[j] === "\n") {
+                caret = i - j;
+                break;
+              } else {
+                caret = i + 1;
+              }
+            }
+            nextText = textarea.value.slice(i + 1).replace(/[\n].*/g, "").length;
+            if (nextText < caret && textarea.selectionStart + caret > i + nextText) {
+              textarea.selectionStart += i - textarea.selectionStart + nextText + 1;
+            } else {
+              textarea.selectionStart += caret;
+            }
+            break;
+          }
+        }
+        break;        
 
       default:
         if (textarea.selectionStart === textarea.selectionEnd) {
@@ -147,7 +218,34 @@ document.addEventListener("keydown", (event) => {
       document.querySelector(".indicator").classList.toggle("key-up");
       break;  
 
+    case "AltLeft":
+      document.querySelector(".indicator").classList.toggle("key-up");
+      break;    
+
     default:
       console.log(event.code);
+  }
+});
+
+
+// Language switcher
+
+document.onkeydown = (event) => {
+  if (event.code == "ControlLeft") {
+    document.onkeyup = (event) => {
+      if (event.code == "AltLeft") {           
+        console.log("lang swith");
+        event.preventDefault();
+      }
+    };
+  }
+};
+
+
+// Light mode for keys
+
+LightMode.addEventListener("click", () => {  
+  for (let i = 0; i < keyBtns.length; i++) {
+    keyBtns[i].classList.toggle("key-light");
   }
 });
